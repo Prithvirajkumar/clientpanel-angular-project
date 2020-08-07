bafaf8a2d4c5f812cc2a8bfc6ef4201fdf4ab48a
Prithvi <prithvirajkumar14@gmail.com>
2020-08-07 19:12:00 -0400

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDMtkxZWHHNIAIPSDsgvRxU26bfyV_gzkk",
-    authDomain: "traversy-ng-clientpan-project.firebaseapp.com",
-    databaseURL: "https://traversy-ng-clientpan-project.firebaseio.com",
-    projectId: "traversy-ng-clientpan-project",
-    storageBucket: "traversy-ng-clientpan-project.appspot.com",
-    messagingSenderId: "505820163101",
-    appId: "1:505820163101:web:290401e912b2f10313eaf4",
-    measurementId: "G-GYVYJ6394Y"
   }
 };
